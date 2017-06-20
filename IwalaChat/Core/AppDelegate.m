e6fc9ff4a263d0165c2fbd425c0066bf36be586b
liwumin <464343235@qq.com>
2017-06-20 11:30:55 +0800

    push

--- IwalaChat/Core/AppDelegate.m
+++ IwalaChat/Core/AppDelegate.m
@@ -22,6 +22,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [TestViewController new];
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 @end
